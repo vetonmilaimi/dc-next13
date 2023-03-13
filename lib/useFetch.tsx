de50eb5187c022fcf54d07bf1ba9e1c5f2b6d2e9
vetonmilaimi <vetonmila@gmail.com>
2023-03-13 22:38:47 +0100

    Fix useFetch hook with env variable

--- lib/useFetch.tsx
+++ lib/useFetch.tsx
@@ -1,4 +1,5 @@
 export default async function useFetch(url: string = "", cache: RequestCache = "force-cache") {
-  const res = await fetch(`http:localhost:3001/${url}`, { cache: cache }); // this should check for the revalidate to {next: {revalidate: 1}}
+  const envUrl = process.env.REACT_APP_API_URL
+  const res = await fetch(`${envUrl}/${url}`, { cache: cache }); // this should check for the revalidate to {next: {revalidate: 1}}
   return res.json();
 }
