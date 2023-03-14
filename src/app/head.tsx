23a16a043201adfaa26e0eb10746526ece9f6898
vetonmilaimi <vetonmila@gmail.com>
2023-03-14 10:25:47 +0100

    Fix og:type

--- src/app/head.tsx
+++ src/app/head.tsx
@@ -6,7 +6,7 @@ export default function Head() {
       <meta name="description" content="Ne e njohim mirë gjuhën e hekurit, tradita vazhdon, kualiteti na mban..." />
       <meta property="og:url" content="https://duraj.org" />
       <meta property="og:title" content="Duraj-com" />
-      <meta property="og:type" content="company" />
+      <meta property="og:type" content="website" />
       <meta property="og:description" content="Ne e njohim mirë gjuhën e hekurit, tradita vazhdon, kualiteti na mban..." />
       <meta property="og:image" content="https://api2.duraj.org/images/image-1666968365719-772911110.jpeg" />
       <meta property="fb:app_id" content="739170167922102" />
